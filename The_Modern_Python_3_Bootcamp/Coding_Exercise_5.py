5ee78c2de116d0b7e20203615f02b95b2f865a4f
eric-el-errante <85587515+eric-el-errante@users.noreply.github.com>
2022-02-27 21:14:05 -0700

    Completed Coding Exercise 5

---
+++ The_Modern_Python_3_Bootcamp/Coding_Exercise_5.py
@@ -0,0 +1,13 @@
+# Set the message variable equal to any string containing a new-line
+# escape sequence
+message = "hello\nthere"
+
+# Add a string to the mountains variable that when printed results in:
+# /\/\/\
+mountains = "/\\/\\/\\"
+
+# Set the quatation variable to any string that contains an escaped
+# double quotation marks
+quotation = "Einstein said, \"Time is relative!\""
+
+print(message, mountains, quotation)
