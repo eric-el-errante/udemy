ea460f4653c12390ac6cd65c0b42a551b18b04a3
eric-el-errante <85587515+eric-el-errante@users.noreply.github.com>
2022-03-03 21:23:51 -0700

    Completed Coding Exercise 7

---
+++ The_Modern_Python_3_Bootcamp/Coding_Exercise_7.py
@@ -0,0 +1,14 @@
+# Set the variable called first to your name.
+first = "Eric"
+
+# Set the variable called last to your last name.
+last = "Meyr"
+
+# Then set the variable called formatted that interpolates both using the
+# .format() method.
+# Make sure you follow this pattern:
+#   "First Name: Colt, Last Name: Steele"
+
+formatted = "First Name: {}, Last Name: {}".format(first, last)
+
+print(formatted)
