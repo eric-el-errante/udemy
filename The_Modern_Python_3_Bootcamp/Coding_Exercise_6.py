45ae9c121fba140ce44912957bbd708c6644b315
eric-el-errante <85587515+eric-el-errante@users.noreply.github.com>
2022-03-03 21:05:30 -0700

    Coding Exericse 6 Completed

---
+++ The_Modern_Python_3_Bootcamp/Coding_Exercise_6.py
@@ -0,0 +1,13 @@
+# Set the variable called greeting to some greeting, e.g. "hello".
+greeting = "Hi there"
+
+# Set the variable called name to some name, e.g. "Heisenberg".
+
+name = "Eric"
+
+# Then set the variable called greet_name so that it concatenates greeting
+# name, and a space " " between them.
+
+greet_name = greeting + " " + name
+
+print(greet_name)
