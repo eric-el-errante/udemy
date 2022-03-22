b665f06a043aa505c2a515729276a48b0998dc64
eric-el-errante <85587515+eric-el-errante@users.noreply.github.com>
2022-03-21 21:10:34 -0600

    Completed Coding Exercise 16

---
+++ The_Modern_Python_3_Bootcamp/Coding_Exercise_016.py
@@ -0,0 +1,19 @@
+# I'm Having a party and made a list of people I want to invite. Unfortunately,
+# I'm a terrible friend and made a couple of spelling errors. Help me
+# correct them
+# change "Hanna" to "Hannah"
+# Change "Geoffrey" to "Jeffry"
+# Change "aparna" to "Aparna"
+
+# DON'T TOUCH THIS PLEASE!
+people = ["Hanna","Louisa","Claudia", "Angela","Geoffrey", "aparna"]
+# DON'T TOUCH THIS PLEASE!
+
+#Change "Hanna" to "Hannah"
+people[0] = "Hannah"
+
+#Change "Geoffrey" to "Jeffrey"
+people[4] = "Jeffrey"
+
+#Change "aparna" to "Aparna" (capitalize it)
+people[5] = "Aparna"
