808e7df56e9908753cde97bed6be22166fe8a767
eric-el-errante <85587515+eric-el-errante@users.noreply.github.com>
2022-03-21 22:17:00 -0600

    Completed Coding Exercise 18

--- The_Modern_Python_3_Bootcamp/Coding_Exercise_018.py
+++ The_Modern_Python_3_Bootcamp/Coding_Exercise_018.py
@@ -1,14 +1,21 @@
 # Create a list called instructors
-instructors = []
+# Add the following strings to the instructors list "Colt" "Blue" "Lisa"
 
-# Add the following strings to the instructors list 
-    # "Colt"
-    # "Blue"
-    # "Lisa"
+# Do it using append method
+instructors = []
 instructors.append("Colt")
 instructors.append("Blue")
 instructors.append("Lisa")
+print(instructors)
 
+# Do it using extend method
+instructors = []
+instructors.extend(["Colt", "Blue", "Lisa"])
+print(instructors)
 
-# Run the tests to make sure you've done this correctly!
+# Do it using insert method
+instructors = []
+instructors.insert(0, "Colt")
+instructors.insert(1, "Blue")
+instructors.insert(2, "Lisa")
 print(instructors)
